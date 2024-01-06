f56c8f0ef9b8410be3def844cb286188af10a3c2
kurusugawa <kurusugawa@DESKTOP-KPBQL0P>
2024-01-06 17:14:08 +0900

    プロジェクト ファイルを追加します。

---
+++ UDPTestTool/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace UDPTestTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReceiveDataText = new System.Windows.Forms.RichTextBox();
+            this.ButtonStop = new System.Windows.Forms.Button();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ReceiveDataText
+            // 
+            this.ReceiveDataText.Location = new System.Drawing.Point(13, 13);
+            this.ReceiveDataText.Name = "ReceiveDataText";
+            this.ReceiveDataText.Size = new System.Drawing.Size(774, 438);
+            this.ReceiveDataText.TabIndex = 0;
+            this.ReceiveDataText.Text = "";
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Location = new System.Drawing.Point(615, 457);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(171, 60);
+            this.ButtonStop.TabIndex = 1;
+            this.ButtonStop.Text = "ストップ";
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(438, 457);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(171, 60);
+            this.ButtonStart.TabIndex = 2;
+            this.ButtonStart.Text = "スタート";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 618);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.ReceiveDataText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ReceiveDataText;
+        private System.Windows.Forms.Button ButtonStop;
+        private System.Windows.Forms.Button ButtonStart;
+    }
+}
+
---
+++ UDPTestTool/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDPTestTool
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer _timer = new System.Windows.Forms.Timer();
+        private IPEndPoint _target = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            using (StreamWriter writer = new StreamWriter(@"./log.txt", false, Encoding.GetEncoding("Shift_JIS")))
+            {
+                writer.WriteLine("Start");
+            }
+
+            _timer.Interval = 100;
+            _timer.Tick += _timer_Tick;
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            byte[] receiveData = { };
+            bool receiveResult = Receive(ref receiveData);
+            if (receiveResult)
+            {
+                using (StreamWriter writer = new StreamWriter(@"./log.txt", true, Encoding.GetEncoding("Shift_JIS")))
+                {
+                    string writeData = string.Empty;
+                    foreach(byte data in receiveData)
+                    {
+                        writeData = writeData + " " + data.ToString("X2");
+                    }
+                    writer.WriteLine(writeData + "\r\n");
+                    ReceiveDataText.Text = writeData;
+
+                    bool sendResult1 = Send(receiveData);
+                    if (sendResult1)
+                    {
+                        writer.WriteLine("SendData--------------------------------------------------------------------------");
+                    }
+                    else
+                    {
+                        writer.WriteLine("Failure SendData------------------------------------------------------------------");
+                        ReceiveDataText.Text = "Failure SendData";
+                    }
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(@"./log.txt", true, Encoding.GetEncoding("Shift_JIS")))
+                {
+                    writer.WriteLine("Failure Receive-----------------------------------------------------------------------");
+                    ReceiveDataText.Text = "Failure Receive";
+                }
+            }
+        }
+
+        private bool Send(byte[] buffer)
+        {
+            try
+            {
+                using (UdpClient udpSend = new UdpClient(20501))
+                {
+                    udpSend.Send(buffer, buffer.Length, _target);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool Receive(ref byte[] buffer)
+        {
+            IPEndPoint remoteEP = null;//任意の送信元からのデータを受信
+            try
+            {
+                using (UdpClient udpReceive = new UdpClient(20501)) 
+                {
+                    udpReceive.Client.ReceiveTimeout = 100;
+                    buffer = udpReceive.Receive(ref remoteEP);
+                    if(_target == null)
+                    {
+                        _target = new IPEndPoint(remoteEP.Address, 20501);
+                    }
+                }
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            if(_timer.Enabled != true)
+            {
+                _timer.Start();
+            }
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            if (_timer.Enabled == true)
+            {
+                _timer.Stop();
+            }
+        }
+    }
+}
